4fd363b380defa999e04b932d452463c9ba2e65c
Igor Morozov <morozofffmia@gmail.com>
2020-07-18 12:32:44 +0300

    command logic

--- src/ml/govnoed/Manhunt/Main.java
+++ src/ml/govnoed/Manhunt/Main.java
@@ -1,15 +1,21 @@
 package ml.govnoed.Manhunt;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
-import org.bukkit.event.block.Action;
 import org.bukkit.event.player.PlayerInteractEvent;
 import org.bukkit.event.player.PlayerMoveEvent;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class Main extends JavaPlugin implements Listener {
+	
+	public Map<String, Player> speedrunners = new HashMap<String, Player>();
+	public Map<String, Player> hunters = new HashMap<String, Player>();
 	
 	@Override
 		public void onEnable() {
@@ -23,8 +29,20 @@ public class Main extends JavaPlugin implements Listener {
 
 		public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
 
+			if (label.equalsIgnoreCase("manhunt")) {
+				if (!(sender instanceof Player)) {
+					sender.sendMessage("Sorry! This command is players only!");
+					return true;
+				}
+				if (args[0].equalsIgnoreCase("victim")) {
+					// join here
+				}
+				if (args[0].equalsIgnoreCase("leave")) {
+					// leave
+				}
+			}
+			
 			return false;
-
 		}
 		
 		
