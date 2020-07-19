e0e39ea0af39c9e53d644f0ddd22084ce6105ce0
Igor Morozov <morozofffmia@gmail.com>
2020-07-19 22:33:09 +0300

    we made it work
    Co-Authored-By: shibatsui <68383417+shibatsui@users.noreply.github.com>

--- src/ml/govnoed/Manhunt/Main.java
+++ src/ml/govnoed/Manhunt/Main.java
@@ -1,24 +1,35 @@
 package ml.govnoed.Manhunt;
 
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 import org.bukkit.Bukkit;
 import org.bukkit.Location;
+import org.bukkit.Material;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
+import org.bukkit.enchantments.Enchantment;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
+import org.bukkit.event.block.Action;
 import org.bukkit.event.entity.PlayerDeathEvent;
 import org.bukkit.event.player.PlayerInteractEvent;
+import org.bukkit.inventory.ItemFlag;
+import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.meta.CompassMeta;
+import org.bukkit.inventory.meta.ItemMeta;
 import org.bukkit.plugin.java.JavaPlugin;
 import org.bukkit.scheduler.BukkitRunnable;
+
+import net.md_5.bungee.api.ChatColor;
 
 public class Main extends JavaPlugin implements Listener {
 	
 	public Map<String, Location[]> victims = new HashMap<String, Location[]>();
-	public Map<String, Location[]> hunters = new HashMap<String, Location[]>();
+	public Map<String, String> hunters = new HashMap<String, String>();
 	public int counter = 0;
 	
 	@Override
@@ -59,8 +70,30 @@ public class Main extends JavaPlugin implements Listener {
 			}
 			if (args[0].equalsIgnoreCase("start")) {
 				
+<<<<<<< Updated upstream
 				runGame();
 				Bukkit.getServer().broadcastMessage("The game will start soon!");
+=======
+				for (Player player : Bukkit.getOnlinePlayers()) {
+					if (!(victims.containsKey(player.getName()))) {
+						hunters.put(player.getName(), "jopa");
+						player.sendMessage("You are the hunter!");
+					} else {
+						player.sendMessage("You are the victim!");
+					}
+				}
+				
+				gameActive = true;
+				runGame();
+				Bukkit.getServer().broadcastMessage("The game will start soon!");
+				
+				
+				
+			}
+			if (args[0].equalsIgnoreCase("stop")) {
+				gameActive = false;
+				Bukkit.getServer().broadcastMessage(ChatColor.RED + "The game is ended!");
+>>>>>>> Stashed changes
 			}
 		}
 		
@@ -73,6 +106,16 @@ public class Main extends JavaPlugin implements Listener {
 			
 			@Override
 			public void run() {
+				
+				for (Player player : Bukkit.getOnlinePlayers()) {
+					if (hunters.containsKey(player.getName())) {
+						player.getInventory().addItem(getCompass());
+					}
+				}
+				
+				Bukkit.getServer().broadcastMessage("Hunters: " + hunters.keySet());
+				Bukkit.getServer().broadcastMessage("Victims: " + victims.keySet());
+				
 				
 				gameRunning();
 				
@@ -118,14 +161,52 @@ public class Main extends JavaPlugin implements Listener {
 	
 	
 	
+	@SuppressWarnings("deprecation")
 	@EventHandler
 	public void onClick(PlayerInteractEvent event) {
-		// event.getAction() == Action.RIGHT_CLICK_AIR
+		if (event.getAction() == Action.RIGHT_CLICK_AIR) {
+			Bukkit.getServer().broadcastMessage("жопа правой кнопкой по воздуху");
+			Player player = event.getPlayer();
+			if (hunters.containsKey(player.getName())) {
+				Bukkit.getServer().broadcastMessage("жопа хантера");
+				if (player.getInventory().getItemInMainHand().getType().equals(Material.COMPASS)) {
+					Bukkit.getServer().broadcastMessage("компас в жопе");
+					if (player.getInventory().getItemInMainHand().getItemMeta().hasLore()) {
+						Bukkit.getServer().broadcastMessage("компас не на жопу");
+						
+						Location[] loc = victims.get("Shibatsui");
+						
+						player.setCompassTarget(loc[counter]);
+					}
+				}
+			}
+		}
 	}
 	
 	@EventHandler
 	public void onDeath(PlayerDeathEvent event) {
 		// check deaths
+	}
+	
+	public ItemStack getCompass() {
+		ItemStack compass = new ItemStack(Material.COMPASS);
+		
+		ItemMeta meta = compass.getItemMeta();
+		
+		meta.setDisplayName("Hunter's compass");
+		List<String> lore = new ArrayList<String>();
+		lore.add("");
+		lore.add(ChatColor.translateAlternateColorCodes('&', "&7(R) &5Find victim"));
+		meta.setLore(lore);
+		
+		meta.addEnchant(Enchantment.MULTISHOT, 1, true);
+		meta.addItemFlags(ItemFlag.HIDE_ENCHANTS);
+		meta.addItemFlags(ItemFlag.HIDE_ATTRIBUTES);
+		meta.setUnbreakable(true);
+		
+		compass.setItemMeta(meta);
+		
+		return compass;
 	}
 
 }
