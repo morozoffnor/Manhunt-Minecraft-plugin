a37f747a50d04fa8260b1fdfbb707deeed837157
Shibatsui <68383417+Shibatsui@users.noreply.github.com>
2020-07-21 00:14:17 +0300

    fixed TabCompleter
    Co-Authored-By: Igor Morozov <24278723+morozoffnor@users.noreply.github.com>

--- src/ml/govnoed/Manhunt/Tab.java
+++ src/ml/govnoed/Manhunt/Tab.java
@@ -9,23 +9,34 @@ import org.bukkit.command.TabCompleter;
 
 public class Tab implements TabCompleter {
 	
-	List<String> arguments = new ArrayList<String>();
+	List<String> arguments1 = new ArrayList<String>();
+	List<String> arguments2 = new ArrayList<String>();
 	
 	public List<String> onTabComplete(CommandSender sender, Command cmd, String label, String[] args) {
 		
-		if (arguments.isEmpty()) {
-			arguments.add("victim");
-			arguments.add("start");
-			arguments.add("leave");
-			arguments.add("settings");
-			arguments.add("stop");
+		if (arguments1.isEmpty()) {
+			arguments1.add("victim");
+			arguments1.add("start");
+			arguments1.add("leave");
+			arguments1.add("settings");
+			arguments1.add("stop");
+		}
+		if (arguments2.isEmpty()) {
+			arguments2.add("headStart");
+			arguments2.add("compassDelay");
 		}
 		
-		
 		List<String> result = new ArrayList<String>();
-		if (args.length == 1) {
-			for (String a : arguments) {
+		switch(args.length) {
+		case 1:
+			for (String a : arguments1) {
 				if (a.toLowerCase().startsWith(args[0].toLowerCase()))
+					result.add(a);
+			}
+			return result;
+		case 2:
+			for (String a : arguments2) {
+				if (a.toLowerCase().startsWith(args[1].toLowerCase()))
 					result.add(a);
 			}
 			return result;
